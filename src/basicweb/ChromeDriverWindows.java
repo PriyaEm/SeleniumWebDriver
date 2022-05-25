27fdddb36a3399e6615d1da2a5efe98c08def52e
priya <priya.medalist@gmail.com>
2022-05-25 12:12:46 +0530

    added handle dropdown

--- src/basicweb/ChromeDriverWindows.java
+++ src/basicweb/ChromeDriverWindows.java
@@ -7,7 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ChromeDriverWindows {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\priya\\eclipse-workspace\\libs\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\priya\\eclipse-workspace\\libs\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		String baseURL = "http://learn.letskodeit.com";
 		driver.get(baseURL);
---
+++ src/basicweb/HandleDropdown.java
@@ -0,0 +1,21 @@
+package basicweb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HandleDropdown {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		String baseURL = "https://courses.letskodeit.com/practice";
+		driver.get(baseURL);
+		
+		
+		Select select = new Select(driver.findElement(By.id("carselect")));
+		select.selectByVisibleText("Benz");
+		select.selectByVisibleText("Honda");
+	}
+
+}
